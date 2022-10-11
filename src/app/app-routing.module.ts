9acde5ae45574c9089451f0e91a57eccb94bc1e5
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-11 14:44:17 -0300

    feat: route created for Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from "./components/nav/nav.component";
 import { TechnicianListComponent } from "./components/technician-list/technician-list.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
