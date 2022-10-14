9c837cd25db3693dc5b03cfb1dcc7a191acf4c51
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-14 10:22:40 -0300

    feat: route for technician-create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { NavComponent } from "./components/nav/nav.component";
 import { TechnicianListComponent } from "./components/technician-list/technician-list.component";
 import { LoginComponent } from "./components/login/login.component";
 import { AuthGuard } from "./auth/auth.guard";
+import {TechnicianCreateComponent} from "./components/technician/technician-create/technician-create.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
-      { path: 'technician', component: TechnicianListComponent }
+
+      { path: 'technician', component: TechnicianListComponent },
+      { path: 'technician/create', component: TechnicianCreateComponent }
     ]
   }
 ];
