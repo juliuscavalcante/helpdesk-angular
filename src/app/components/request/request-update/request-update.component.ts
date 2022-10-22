5b144906626f2f00bb517e58bdad567d08ad47d3
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-22 14:48:07 -0300

    feat: starting request-update implementation

--- src/app/components/request/request-update/request-update.component.ts
+++ src/app/components/request/request-update/request-update.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Request } from "../../../model/request";
+import { Customer } from "../../../model/customer";
+import { Technician } from "../../../model/technician";
+import { FormControl, Validators } from "@angular/forms";
+import { RequestService } from "../../../service/request.service";
+import { CustomerService } from "../../../service/customer.service";
+import { TechnicianService } from "../../../service/technician.service";
+import { ToastrService}  from "ngx-toastr";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-update-request',
@@ -7,9 +16,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestUpdateComponent implements OnInit {
 
-  constructor() { }
+  request: Request = {
+    priority:       '',
+    status:         '',
+    title:          '',
+    notes:          '',
+    technician:     '',
+    customer:       '',
+    customerName:   '',
+    technicianName: '',
+  }
+
+  customers: Customer[] = []
+  technicians: Technician[] = []
+
+  priority: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  title: FormControl = new FormControl(null, [Validators.required])
+  notes: FormControl = new FormControl(null, [Validators.required])
+  technician: FormControl = new FormControl(null, [Validators.required])
+  customer: FormControl = new FormControl(null, [Validators.required])
+
+  constructor(
+      private requestService: RequestService,
+      private customerService: CustomerService,
+      private technicianService: TechnicianService,
+      private toastService: ToastrService,
+      private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.findAllCustomers();
+    this.findAllTechnician();
+  }
+
+  create(): void {
+    this.requestService.create(this.request).subscribe(request => {
+      this.toastService.success('Request created successfully', 'New Request');
+      this.router.navigate(['requests']);
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  findAllCustomers(): void {
+    this.customerService.findAll().subscribe(request => {
+      this.customers = request;
+    })
+  }
+
+  findAllTechnician(): void {
+    this.technicianService.findAll().subscribe(request => {
+      this.technicians = request;
+    })
+  }
+
+  fieldValidation(): boolean {
+    return this.priority.valid &&
+        this.status.valid &&
+        this.title.valid &&
+        this.notes.valid &&
+        this.technician.valid &&
+        this.customer.valid
   }
 
 }
