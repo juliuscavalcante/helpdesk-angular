e2c3501cf583a5dc12d2d58e2ca2957caef6f649
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-22 14:43:23 -0300

    refactor: rename components

---
+++ src/app/components/request/request-update/request-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-request',
+  templateUrl: './request-update.component.html',
+  styleUrls: ['./request-update.component.css']
+})
+export class RequestUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
