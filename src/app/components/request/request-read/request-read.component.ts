2226d51ab58f055161efe32a3e5efea97e88fcba
Julius Cavalcante <julius.c@outlook.com.br>
2022-10-23 13:07:14 -0300

    feat: finished request-read component

--- src/app/components/request/request-read/request-read.component.ts
+++ src/app/components/request/request-read/request-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Request } from "../../../model/request";
+import { RequestService } from "../../../service/request.service";
+import { ToastrService } from "ngx-toastr";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-request-read',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestReadComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  request: Request = {
+    priority:       '',
+    status:         '',
+    title:          '',
+    notes:          '',
+    technician:     '',
+    customer:       '',
+    customerName:   '',
+    technicianName: '',
   }
 
+  constructor(
+      private requestService: RequestService,
+      private toastService: ToastrService,
+      private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.request.id = this.route.snapshot.paramMap.get('id');
+  }
+
+  findById(): void {
+    this.requestService.findById(this.request.id).subscribe(request => {
+      this.request = request;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  returnStatus(status: any): string {
+    if (status == '0') {
+      return 'OPEN'
+    } else if  (status == '1') {
+      return 'PROGRESS'
+    } else {
+      return 'CLOSED'
+    }
+  }
+
+  returnPriority(priority: any): string {
+    if (priority == '0') {
+      return 'LOW'
+    } else if  (priority == '1') {
+      return 'MEDIUM'
+    } else {
+      return 'HIGH'
+    }
+  }
 }
